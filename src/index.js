54cb113b95597a3ad4a20b45c92a0a38426374dc
Vastia <zmfwl123@gmail.com>
2022-05-12 14:20:51 +0900

    기본 문법 활용 예제 1)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<p className="start">안녕 리액트!</p>);
+root.render(
+  <>
+    <h1 id="title">가위바위보</h1>
+    <button className="hand">가위</button>
+    <button className="hand">바위</button>
+    <button className="hand">보</button>
+  </>
+);
