bb201dc4460fa751d915214332debae2556897a4
Vastia <zmfwl123@gmail.com>
2022-05-12 14:33:43 +0900

    html 안에 JS 문법 넣기 예제

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<p className="start">안녕 리액트!</p>);
+const product = "Macbook";
+const imgUrl =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/MacBook_with_Retina_Display.png/500px-MacBook_with_Retina_Display.png";
+function handclick(e) {
+  alert("곧 도착합니다.");
+}
+
+root.render(
+  <>
+    <h1>나만의 {product} 주문하기</h1>
+    <img src={imgUrl}></img>
+    <button onClick={handclick}>확인</button>
+  </>
+);
