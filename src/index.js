25079d55192528c91b90896f6c175f8f505a8119
Vastia <zmfwl123@gmail.com>
2022-05-12 15:03:02 +0900

    가위바위보 함수 추가
    함수 추가 및 활용할 변수 추가

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const me = "rock";
+const other = "scissors";
+const WINS = {
+  rock: "scissors",
+  scissors: "paper",
+  paper: "rock",
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return "승리";
+  else if (left === WINS[right]) return "패배";
+  else {
+    return "무승부";
+  }
+}
+function handleClick(e) {
+  console.log("가위바위보!");
+}
+
 root.render(
   <>
     <h1 id="title">가위바위보</h1>
-    <button className="hand">가위</button>
-    <button className="hand">바위</button>
-    <button className="hand">보</button>
+    <h2>{getResult(me, other)}</h2>
+    <button onClick={handleClick} className="hand">
+      가위
+    </button>
+    <button onClick={handleClick} className="hand">
+      바위
+    </button>
+    <button onClick={handleClick} className="hand">
+      보
+    </button>
   </>
 );
